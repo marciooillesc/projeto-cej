3fe1348fe91576c495b8461305f8713817166e2f
iprpr <marciooillesc@gmail.com>
2026-04-01 17:04:41 -0300

    Add files via upload

--- modules/ia.js
+++ modules/ia.js
@@ -1,11 +1,8 @@
-export async function gerarSimuladoIA(dados) {
-  const res = await fetch("https://gdjpwwmdfssvokuuibwz.supabase.co/functions/v1/gerar-simulado", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
+export async function gerarSimuladoIA(dados){
+  const res = await fetch("https://SEU-PROJETO.supabase.co/functions/v1/gerar-simulado",{
+    method:"POST",
+    headers:{ "Content-Type":"application/json" },
     body: JSON.stringify(dados)
   });
-
   return await res.json();
 }
